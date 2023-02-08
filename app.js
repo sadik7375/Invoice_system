e8b28db766550f040f68b4b02ca8c7091238dc91
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2023-02-08 16:03:39 +0600

    invoice system

---
+++ app.js
@@ -0,0 +1,56 @@
+function GetPrint() {
+    window.print();
+
+
+}
+
+function BtnAdd() {
+    var v = $("#TRow").clone().appendTo("#TBody");
+    $(v).find('input').val('');
+    $(v).removeClass('d-none');
+
+}
+
+function BtnDel(v) {
+
+    $(v).parent().parent().remove();
+    GetTotal()
+
+
+}
+
+function Calc(v) {
+
+    var index = $(v).parent().parent().index();
+    var qty = document.getElementsByName("qty")[index].value;
+
+
+    var rate = document.getElementsByName("rate")[index].value;
+
+
+
+    var amt = qty * rate;
+
+    document.getElementsByName("amt")[index].value = amt;
+    GetTotal();
+}
+
+function GetTotal() {
+    var sum = 0;
+
+    var amts = document.getElementsByName("amt");
+    for (let index = 0; index < amts.length; index++) {
+        var amt = amts[index].value;
+        sum = +(sum) + +(amt);
+    }
+    document.getElementById("Ftotal").value = sum;
+
+    var tax = document.getElementById("Ftax").value;
+    var taxRate = (sum * tax) / 100;
+
+    var net = +(sum) + +(taxRate);
+    document.getElementById("Fnet").value = net;
+
+
+
+}
